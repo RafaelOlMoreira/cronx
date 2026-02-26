2188d4eccac4cfdd8e50b686437458d00d066adc
Rafael Oliveira Moreira <138674189+RafaelOlMoreira@users.noreply.github.com>
2026-02-26 20:33:26 -0300

    feat: Responsive layout created.

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -29,9 +29,24 @@ function Header() {
 
     return (
         <>
-            <header className='fixed z-3000 flex items-center w-full bg-black/40 px-5 justify-between'>
+            <header className='fixed z-3000 flex items-center w-full bg-black/80 px-5 md:px-10 xl:px-20 justify-between'>
                 <img src={Logo} className='h-15' />
+                <div className='hidden lg:flex w-full'>
+                    <ul className='flex w-full items-center gap-10 justify-center text-white text-md'>
+                        <li><a className='hover:cursor-pointer' href='#Home'>Home</a></li>
+                        <li><a className='hover:cursor-pointer' href='#'>Services</a></li>
+                        <li><a className='hover:cursor-pointer' href='#'>Solutions</a></li>
+                        <li><a className='hover:cursor-pointer' href='#'>Portifolio</a></li>
+                        <li><a className='hover:cursor-pointer' href='#'>Pricing</a></li>
+                        <li><a className='hover:cursor-pointer' href='#'>About</a></li>
+                        <li><a className='hover:cursor-pointer' href='#'>Contact</a></li>
+                    </ul>
+                    <button className='mr-auto bg-white p-2 px-12 rounded-lg transition duration-300 hover:scale-105 hover:cursor-pointer'>
+                        <span className='text-black text-md text-nowrap'>Get a Quote</span>
+                    </button>
+                </div>
                 <button
+                    className='block lg:hidden'
                     onClick={toggleMenu}
                     aria-expanded={navMenuAberto}
                 >
@@ -43,23 +58,17 @@ function Header() {
                 </button>
             </header>
 
-            <div
-                ref={overlayRef}
-                className={`fixed inset-0 z-2000 bg-black/95 transition duration-300 
-                ${navMenuAberto ? "opacity-100 pointer-events-auto fixed" : "hidden pointer-events-none"}`}>
-                <div className={`flex justify-center h-screen items-center transform transition-transform duration-300
-                                ${navMenuAberto ? 'translate-y-0' : 'translate-y-4'
-
-                    }`}>
+            <div ref={overlayRef} className={`fixed inset-0 z-2000 bg-black/95 transition duration-300 ${navMenuAberto ? "opacity-100 pointer-events-auto fixed" : "hidden pointer-events-none"}`}>
+                <div className="flex justify-center h-screen items-center transform transition-transform duration-300">
                     <ul className='text-white text-center space-y-5 text-xl'>
-                        <li><span className='hover:cursor-pointer' id='#Home' onClick={toggleMenu}>Home</span></li>
-                        <li><span className='hover:cursor-pointer' id='#' onClick={toggleMenu}>Services</span></li>
-                        <li><span className='hover:cursor-pointer' id='#' onClick={toggleMenu}>Solutions</span></li>
-                        <li><span className='hover:cursor-pointer' id='#' onClick={toggleMenu}>Portifolio</span></li>
-                        <li><span className='hover:cursor-pointer' id='#' onClick={toggleMenu}>Pricing</span></li>
-                        <li><span className='hover:cursor-pointer' id='#' onClick={toggleMenu}>About</span></li>
-                        <li><span className='hover:cursor-pointer' id='#' onClick={toggleMenu}>Contact</span></li>
-                        <button className='bg-white p-4 px-20 rounded-lg transition duration-300 hover:scale-105'>
+                        <li><a className='hover:cursor-pointer' href='#Home' onClick={toggleMenu}>Home</a></li>
+                        <li><a className='hover:cursor-pointer' href='#' onClick={toggleMenu}>Services</a></li>
+                        <li><a className='hover:cursor-pointer' href='#' onClick={toggleMenu}>Solutions</a></li>
+                        <li><a className='hover:cursor-pointer' href='#' onClick={toggleMenu}>Portifolio</a></li>
+                        <li><a className='hover:cursor-pointer' href='#' onClick={toggleMenu}>Pricing</a></li>
+                        <li><a className='hover:cursor-pointer' href='#' onClick={toggleMenu}>About</a></li>
+                        <li><a className='hover:cursor-pointer' href='#' onClick={toggleMenu}>Contact</a></li>
+                        <button className='bg-white p-4 px-20 rounded-lg transition duration-300 hover:scale-105 hover:cursor-pointer'>
                             <span className='text-black text-lg'>Get a Quote</span>
                         </button>
                     </ul>
