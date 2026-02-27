ad64cd04cdb7009902199ea765add1610369a258
Rafael Oliveira Moreira <138674189+RafaelOlMoreira@users.noreply.github.com>
2026-02-27 20:17:55 -0300

    feat: Responsive desktop layout created.

--- src/pages/Solutions.jsx
+++ src/pages/Solutions.jsx
@@ -4,16 +4,19 @@ function Solutions() {
 
     return (
         <>
-            <section className="bg-plus-pattern bg-[#0F172A] min-h-auto text-white px-5 pt-20 pb-10">
+            <section className="bg-plus-pattern bg-[#0F172A] min-h-auto text-white px-5 lg:px-20 pt-20 pb-10">
                 <div className='space-y-2 mb-15'>
                     <p className='text-[#b7bac0] text-md'>OUR PROCESS</p>
                     <h1 className='text-4xl text-white font-bold'>From Concept to Launch</h1>
                 </div>
 
-                <div className='flex flex-col space-y-15'>
+                <div className='flex flex-col lg:flex-row space-y-15 lg:space-y-0 lg:gap-15'>
                     {data.map((d) => (
                         <div className='space-y-4'>
-                            <p className='w-min text-white font-bold text-3xl border p-4 rounded-full'>{d.number}</p>
+                            <div className='relative'>
+                                <div className='hidden lg:block absolute bg-white/20 w-full h-0.5 mt-8.5' />
+                                <p className='lg:relative w-min bg-[#0F172A] text-white font-bold text-3xl border p-4 rounded-full'>{d.number}</p>
+                            </div>
                             <h2 className='text-white text-2xl font-semibold'>{d.title}</h2>
                             <p className='text-md'>{d.description}</p>
                         </div>
